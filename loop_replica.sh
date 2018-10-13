be18660ae6540ac2fb134b346c2f1a24d7bd38d6
vincenzobaz <bazzucchi.vincenzo@gmail.com>
2018-10-13 18:12:25 +0200

    Simplify script

--- loop_replica.sh
+++ loop_replica.sh
@@ -3,7 +3,7 @@
 comm="java -jar Replica/target/scala-2.12/ReplicaManager.jar $1 $2 $((7000 + $1)) http://127.0.0.1:9090"
 while true
 do
-    eval ${comm}
+    $comm
     if [ $? -eq 0 ]; then
         break
     fi
